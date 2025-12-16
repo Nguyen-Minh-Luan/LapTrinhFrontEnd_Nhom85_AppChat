69e9789637c3d82fcdf8f852f823395f4a90e8fc
6789zkm <22130102@st.hcmuaf.edu.vn>
2025-12-16 23:26:49 +0700

    idk
    Signed-off-by: 6789zkm <22130102@st.hcmuaf.edu.vn>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      xin chào
-    </div>
-  );
+  return <div className="App">xin chào</div>;
 }
 
 export default App;
